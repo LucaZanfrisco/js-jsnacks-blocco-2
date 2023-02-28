f5605b6ea3aae695bb3e11526e5e18f40794357b
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-02-28 16:22:05 +0100

    correzione lunghezza array dei cognomi

--- snack-3/main.js
+++ snack-3/main.js
@@ -2,7 +2,7 @@
 
 // Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
 
-const nomi = ['luca','marco','mattia']
+const nomi = ['luca','marco','mattia','filippo','antonio'];
 const cognomi = ['pippo','paperino','topolino'];
 
 let  nomeRandom;
@@ -12,7 +12,7 @@ const lista = [];
 
 for(let i = 0; i < 3; i++){
     nomeRandom = nomi[Math.floor(Math.random() * nomi.length)];
-    cognomeRandom = cognomi[Math.floor(Math.random() * nomi.length)]; 
+    cognomeRandom = cognomi[Math.floor(Math.random() * cognomi.length)]; 
     const fullName = `${nomeRandom} ${cognomeRandom}`; 
     lista.push(fullName);
 }
